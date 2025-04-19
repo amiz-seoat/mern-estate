318f480e538a835c59b7cad1211d9522081ebec9
amiz-seoat <abduselammiz6@gmail.com>
2025-04-19 04:56:19 -0400

    Add delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,7 +3,7 @@
   import { useRef } from "react";
   import { account, storage } from "../appwrite/appwriteconfig.js";
   //import { Await } from "react-router-dom";
-  import { updateUserStart, updateUserSuccess, updateUserFailure } from "../redux/user/userSlice.js";
+  import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess } from "../redux/user/userSlice.js";
   import { useDispatch } from "react-redux";
   import {ID} from "appwrite";
 
@@ -84,6 +84,24 @@
     }
   }
 
+
+  const handleDeleteUser = async ()=>{
+    try {
+      dispatch(deleteUserStart());
+      const res=await fetch(`/api/user/delete/${currentUser._id}`,{
+        method:'DELETE',
+      })
+      const data = await res.json();
+      if(data.success === false){
+        dispatch(deleteUserFailure(data.message))
+        return;
+      }
+      dispatch(deleteUserSuccess(data))
+    } catch (error) {
+      dispatch(deleteUserFailure(error.messsage))
+    }
+  }
+
     return (
       <div>
         <div className="max-w-lg mx-auto p-3 mY-7">
@@ -138,7 +156,7 @@
             </button>
           </form>
           <div className="flex justify-between mt-4 ">
-            <span className="text-red-800 cursor-pointer">Delete account</span>
+            <span onClick={handleDeleteUser} className="text-red-800 cursor-pointer">Delete account</span>
             <span className="text-red-800 cursor-pointer">Sign out</span>
           </div>
           <p className="text-red-700 mt-5">{error ? error:''}</p>
